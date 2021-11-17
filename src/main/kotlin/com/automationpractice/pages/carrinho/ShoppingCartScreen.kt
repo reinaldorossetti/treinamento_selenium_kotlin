ac31518c51fb6da9174f73f0804cef7cec189df6
Reinaldo Rossetti <reinaldo.rossetti@paygo.com.br>
2021-11-17 15:35:47 -0300

    refatorando os elementos

--- src/main/kotlin/com/automationpractice/pages/carrinho/ShoppingCartScreen.kt
+++ src/main/kotlin/com/automationpractice/pages/carrinho/ShoppingCartScreen.kt
@@ -4,10 +4,10 @@ import com.automationpractice.core.BaseCore
 import org.openqa.selenium.By
 
 open class ShoppingCartScreen: BaseCore() {
-    val elemInputPesquisar = By.id("search_query_top")
-    val elemBtnPesquisar = By.cssSelector("button[name=\"submit_search\"]")
-    val elemLinkAddToCard = By.cssSelector("a[title=\"Printed Summer Dress\"]")
-    val elemLinkCheckout = By.cssSelector("form[id=\"buy_block\"] button[name=\"Submit\"] span")
-    val elemAddToCheckout = By.cssSelector("a[title=\"Proceed to checkout\"]")
-    val elemQuatidadeProduto = By.cssSelector("#cart_title span[id=summary_products_quantity]")
+    val elemInputPesquisar: By = By.id("search_query_top")
+    val elemBtnPesquisar: By = By.cssSelector("button[name=\"submit_search\"]")
+    val elemLinkAddToCard: By = By.cssSelector("a[title=\"Printed Summer Dress\"]")
+    val elemLinkCheckout: By = By.cssSelector("form[id=\"buy_block\"] button[name=\"Submit\"] span")
+    val elemAddToCheckout: By = By.cssSelector("a[title=\"Proceed to checkout\"]")
+    val elemQuatidadeProduto: By = By.cssSelector("#cart_title span[id=summary_products_quantity]")
 }
